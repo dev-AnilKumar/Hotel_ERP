861b2660915b523e8728bf9ee6e4d0aa9b56d1c3
Anil <anilkrsingh1561@gmail.com>
2024-11-21 01:58:26 +0530

    Backend | created product ctrl

--- hotel_backend/index.js
+++ hotel_backend/index.js
@@ -3,22 +3,29 @@ const app = express();
 const dotenv = require('dotenv');
 const dbConnect = require('./config/dbConnect');
 const userRoute = require('./routes/userRoutes');
+const roomRoute = require('./routes/roomRoutes');
+const productRoute = require('./routes/productRoutes');
 const cookieParser = require('cookie-parser');
 
 dotenv.config();
 dbConnect();
+
+const PORT = process.env.PORT || 8000;
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(express.json())
+
+// for reading cookies
 app.use(cookieParser())
 
-
+// api routes
 app.use('/api/v1/user', userRoute)
+app.use('/api/v1/room', roomRoute)
+app.use('/api/v1/product', productRoute)
 
-const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`)
